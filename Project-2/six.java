5350736bf17b56c42805e261cb4449d56f6f748e
kkdave28 <kkdave@uci.edu>
2018-04-13 19:35:26 -0700

    significant changes made to six

--- Project-2/six.java
+++ Project-2/six.java
@@ -5,7 +5,6 @@ import java.lang.*;
 
 class six
 {
-    static int counter = 1;
     public static void main(String args[]) throws IOException
     {
         String wordstr = new String(Files.readAllBytes(Paths.get(args[0]))).replaceAll("[^a-zA-Z0-9]"," ").toLowerCase();
@@ -13,8 +12,7 @@ class six
         List<String> all_words = new ArrayList<String>(Arrays.asList(wordstr.split(" ")));
         List<String> stopwords = new ArrayList<String>(Arrays.asList(swstr.split(" ")));
         HashMap<String,Integer> map = new HashMap<String, Integer>();
-        for(String x: all_words){if(map.containsKey(x) && !stopwords.contains(x)){map.put(x,map.get(x)+1);}else{map.put(x,1);}}
-        map.remove("s");map.remove("");        
+        for(String x: all_words){if(map.containsKey(x) && !stopwords.contains(x)&& x.length()>1){map.put(x,map.get(x)+1);}else{map.put(x,1);}}      
         map.entrySet().stream().sorted((k1, k2) -> -k1.getValue().compareTo(k2.getValue())).limit(25).forEach(k ->{System.out.println(k.getKey() + "  -  " + k.getValue());});
     }
 }
