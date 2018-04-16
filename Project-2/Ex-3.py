9ebc73bb96d2ae587b2d36b7c95a4b3058a16c51
kkdave28 <kkdave@uci.edu>
2018-04-16 14:19:57 -0700

    Started working on Project-3, rules and README.md file laid out.

--- Project-2/Ex-3.py
+++ Project-2/Ex-3.py
@@ -1,53 +1,50 @@
- 1 #!/usr/bin/env python
-  2
-  3 import sys, string
-  4 # the global list of [word, frequency] pairs
-  5 word_freqs = []
-  6 # the list of stop words
-  7 with open('../stop_words.txt') as f:
-  8 stop_words = f.read().split(',')
-  9 stop_words.extend(list(string.ascii_lowercase))
- 10
- 11 # iterate through the file one line at a time
- 12 for line in open(sys.argv[1]):
- 13 start_char = None
- 14 i = 0
- 15 for c in line:
- 16    if start_char == None:
- 17        if c.isalnum():
- 18        # We found the start of a word
- 19        start_char = i
- 20    else:
- 21        if not c.isalnum():
- 22        # We found the end of a word. Process it
- 23        found = False
- 24        word = line[start_char:i].lower()
- 25        # Ignore stop words
- 26        if word not in stop_words:
- 27         pair_index = 0
- 28         # Let's see if it already exists
- 29         for pair in word_freqs:
- 30         if word == pair[0]:
- 31            pair[1] += 1
- 32            found = True
- 33            found_at = pair_index
- 34            break
- 35         pair_index += 1
- 36         if not found:
- 37         word_freqs.append([word, 1])
- 38         elif len(word_freqs)> 1:
- 39         # We may need to reorder
- 40         for n in reversed(range(pair_index)):
- 41            if word_freqs[pair_index][1]>
-              word_freqs[n][1]:
- 42                # swap
- 43                word_freqs[n], word_freqs[
-               pair_index] = word_freqs[
-               pair_index], word_freqs[n]
- 44                pair_index = n
- 45        # Let's reset
- 46        start_char = None
- 47    i += 1
- 48
- 49 for tf in word_freqs[0:25]:
- 50    print tf[0], ' - ', tf[1]
+#!/usr/bin/env python
+
+import sys, string
+# the global list of [word, frequency] pairs
+word_freqs = []
+# the list of stop words
+with open('../stop_words.txt') as f:
+    stop_words = f.read().split(',')
+stop_words.extend(list(string.ascii_lowercase))
+
+# iterate through the file one line at a time 
+for line in open(sys.argv[1]):
+    start_char = None
+    i = 0
+    for c in line:
+        if start_char == None:
+            if c.isalnum():
+                # We found the start of a word
+                start_char = i
+        else:
+            if not c.isalnum():
+                # We found the end of a word. Process it
+                found = False
+                word = line[start_char:i].lower()
+                # Ignore stop words
+                if word not in stop_words:
+                    pair_index = 0
+                    # Let's see if it already exists
+                    for pair in word_freqs:
+                        if word == pair[0]:
+                            pair[1] += 1
+                            found = True
+                            found_at = pair_index
+                            break
+                        pair_index += 1
+                    if not found:
+                        word_freqs.append([word, 1])
+                    elif len(word_freqs) > 1:
+                        # We may need to reorder
+                        for n in reversed(range(pair_index)):
+                            if word_freqs[pair_index][1] > word_freqs[n][1]:
+                                # swap
+                                word_freqs[n], word_freqs[pair_index] = word_freqs[pair_index], word_freqs[n]
+                                pair_index = n
+                # Let's reset
+                start_char = None
+        i += 1
+
+for tf in word_freqs[0:25]:
+    print tf[0], ' - ', tf[1]
---
+++ Project-3/README.md
@@ -0,0 +1,9 @@
+1. This weeks assignment will look at flavours of OOP
+2. 5 different exercises namely 12.1, 12.2, 14.1, 14.2, 15.1
+3. 12.1/12.2 - looks at mapping tasks
+4. 14.1/14.2 - looks at deffering calls (i.e) making a scheduler and calling functions at appropriate times
+5. 15.1 looks at most of the stuff from 14.1 except it is in a pub-sub format.
+6. as usual 5 build.py and run.py files for 5 programs.
+7. make sure to chmod +x the build and run files if permissions are not set.
+8. This project was done entirely on Linux(Ubuntu).
+
