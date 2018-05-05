c5d37e1bafc5bd30827f95cf64114f6b153f652a
kkdave28 <kkdave@uci.edu>
2018-05-04 21:06:11 -0700

    Works now 16.3 is done

--- Project-5/sixteen.py
+++ Project-5/sixteen.py
@@ -2,19 +2,21 @@
 import sys, re, operator, string
 from abc import ABCMeta
 import atexit
+import inspect
+#import sixteen
 #
 # The classes
 #
 class TFExercise():
     __metaclass__ = ABCMeta
 
-    def info(self):
+    def info(self,ignore=None):
         return self.__class__.__name__
 
 class DataStorageManager(TFExercise):
     """ Models the contents of the file """
     
-    def __init__(self, path_to_file):
+    def __init__(self, path_to_file=None):
         with open(path_to_file) as f:
             self._data = f.read()
         pattern = re.compile('[\W_]+')
@@ -30,7 +32,7 @@ class DataStorageManager(TFExercise):
 class StopWordManager(TFExercise):
     """ Models the stop word filter """
     
-    def __init__(self):
+    def __init__(self,ignore=None):
         with open('../stop_words.txt') as f:
             self._stop_words = f.read().split(',')
         # add single-letter words
@@ -45,7 +47,7 @@ class StopWordManager(TFExercise):
 class WordFrequencyManager(TFExercise):
     """ Keeps the word frequency data """
     
-    def __init__(self):
+    def __init__(self, ignore=None):
         self._word_freqs = {}
 
     def increment_count(self, word):
@@ -61,15 +63,15 @@ class WordFrequencyManager(TFExercise):
         print( super(WordFrequencyManager, self).info() + ": My major data structure is a " + self._word_freqs.__class__.__name__ +" \n" +"Methods implemented are:\n" + "- increment_count(self,word)\n"+ "- sorted(self)\n" + "- info(self)\n")
 
 class WordFrequencyController(TFExercise):
-    def __init__(self, path_to_file):
+    def __init__(self, path_to_file=None):
         self._storage_manager = DataStorageManager(path_to_file)
         self._stop_word_manager = StopWordManager()
         self._word_freq_manager = WordFrequencyManager()
-        atexit.register(self._stop_word_manager.info)
-        atexit.register(self._storage_manager.info)
-        atexit.register(self._word_freq_manager.info)
+        #atexit.register(self._stop_word_manager.info)
+        #atexit.register(self._storage_manager.info)
+        #atexit.register(self._word_freq_manager.info)
         #atexit.register(TFExercise.info)
-        atexit.register(self.info)
+        #atexit.register(self.info)
 
     def run(self):
         for w in self._storage_manager.words():
@@ -80,10 +82,21 @@ class WordFrequencyController(TFExercise):
         for (w, c) in word_freqs[0:25]:
             print w, ' - ', c
     def info(self):
-        print(super(WordFrequencyController,self).info() + ": Classes I manage " + self._storage_manager.__class__.__name__ + " " + self._stop_word_manager.__class__.__name__+" "+ self._word_freq_manager.__class__.__name__ +"\n"+"Methods implemented are:\n" + "- run(self)\n"+ "- info(self)\n")
+        print(super(WordFrequencyController,self).info() + ": Classes I manage are " + self._storage_manager.__class__.__name__ + " " + self._stop_word_manager.__class__.__name__+" "+ self._word_freq_manager.__class__.__name__ +"\n"+"Methods implemented are:\n" + "- run(self)\n"+ "- info(self)\n")
 
 #
 # The main function
 #
-WordFrequencyController(sys.argv[1]).run()
-print("\n")
+if __name__ == "__main__":
+    WordFrequencyController(sys.argv[1]).run()
+    print("\n")
+    import sixteen
+    for name, data in inspect.getmembers(sixteen, inspect.isclass):
+        if name == '__builtins__':
+            continue
+        if name != 'ABCMeta' and name != "TFExercise":
+            str = name +"(sys.argv[1]).info()"
+            #print(str)
+            eval(str)
+        #print '%s :' % name, repr(data)
+    
